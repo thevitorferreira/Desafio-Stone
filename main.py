5d9e773ef9708640593042e7644fd5821d49cae6
SuubMatias <vitorlucasbarboza@hotmail.com>
2021-12-21 20:14:09 -0400

    adiconando verificação de exceções

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas as pd     
 
 
-pessoas = ['Vitor', 'Lucas', 'Guilherme', 'Lima']
+pessoas = ['Vitor', 'Lucas', 'Guilherme', 'Lima'] #define a lista de devedores
 
-dados = pd.read_excel("gastos.xlsx")
+dados = pd.read_excel("gastos.xlsx") #importa a planilha de gastos
 
 def divisao_despesa(dados,lista_pessoas):
+    if (len(lista_pessoas)==0) & (not dados.empty): #verifica se a lista está vazia
+        raise 'Impossivel dividir despesas entre 0 pessoas'
     pessoas = {}
-    for pessoa in lista_pessoas:
+    for pessoa in lista_pessoas: #adicionando pessoas ao dicionário
         pessoas[pessoa] = 0
+    if dados.empty: #verifica se a tabela de gastos está vazia
+        return pessoas
+    #calcula o gastos totais
     dados["Valor Total"] = dados["Valor Unitário"]*dados["Quantidade"]
     total_gasto = dados["Valor Total"].sum()
-
+    #separa os reais dos centavos
     quantidade = len(pessoas)
     centavos = int(str(total_gasto).split('.')[1][0:2])
     reais = int(total_gasto/1)
@@ -19,7 +24,7 @@ def divisao_despesa(dados,lista_pessoas):
     centavos_divididos = centavos / quantidade
     centavos_divididos = centavos_divididos + int(str(reais_divididos).split('.')[1])
     reais_divididos = int(str(reais_divididos).split('.')[0])
-
+    #divide e arredonda os gastos
     pagamento_a_mais_ultimo = 0
     if centavos_divididos%1!=0:
         pagamento_a_mais_ultimo = int((centavos_divididos%1) * quantidade)
